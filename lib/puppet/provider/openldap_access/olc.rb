1bdc2e71ebfc2e15000496856ad6c53521777952
Alexander Pánek <alexander@panek.work>
2016-03-17 17:54:24 +0100

    get_paragraphs is also a class method in the parent provider.

--- lib/puppet/provider/openldap_access/olc.rb
+++ lib/puppet/provider/openldap_access/olc.rb
@@ -192,7 +192,7 @@ Puppet::Type.
   end
 
   def get_current(suffix)
-    paragraphs = get_paragraphs(self.class.slapcat('(olcAccess=*)', self.class.getDn(suffix)))
+    paragraphs = self.class.get_paragraphs(self.class.slapcat('(olcAccess=*)', self.class.getDn(suffix)))
 
     paragraphs.collect do |paragraph|
       paragraph.
