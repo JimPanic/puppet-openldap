bb421d113aa6dfa67c1705c0e95ca088feb92fa8
Alexander Pánek <alexander@panek.work>
2016-03-23 17:54:22 +0100

    config_hash spec: Remove one test for Ruby 1.8.x that relies on Hash key order

--- spec/unit/puppet/provider/openldap_config_hash/olc_spec.rb
+++ spec/unit/puppet/provider/openldap_config_hash/olc_spec.rb
@@ -89,9 +89,52 @@ LDIF
     it 'should create an entry in cn=config' do
       provider.stubs(:ldapmodify).returns(0)
 
-      if RUBY_VERSION =~ /^1\.8/
-        expect(provider.create).to eq(create_ldif_ruby_1_8_7)
-      else
+      # NOTE: Skiping this test in Ruby 1.8.x. The order of hash keys is not
+      # defined and cannot be relied upon.
+      # (http://stackoverflow.com/questions/19092185/ruby-hash-by-default-get-sorted-in-ruby-1-8-7)
+      # By checking the created LDIF, the test relies on the order of hash keys
+      # as is in the data passed to Puppet. So there's a 50:50 chance for the
+      # test to fail on Ruby 1.8.x with this test in place with the following error message:
+      #
+      # Failures:
+      #   1) Puppet::Type::Openldap_config_hash::ProviderOlc when creating
+      #   should create an entry in cn=config Failure/Error:
+      #   expect(provider.create).to eq(create_ldif_ruby_1_8_7)
+      #
+      #        expected: "dn: cn=config\nchangetype: modify\nreplace:
+      #        olcTLSCertificateKeyFile\nolcTLSCertificateKeyFile:
+      #        /etc/ssl/private/key.pem\n-\nadd: olcLogLevel\nolcLogLevel:
+      #        stats\n-\nreplace: olcTLSCertificateFile\nolcTLSCertificateFile:
+      #        /etc/ssl/certs/cert.pem\n-\n" got: "dn: cn=config\nchangetype:
+      #        modify\nreplace: olcTLSCertificateFile\nolcTLSCertificateFile:
+      #        /etc/ssl/certs/cert.pem\n-\nreplace:
+      #        olcTLSCertificateKeyFile\nolcTLSCertificateKeyFile:
+      #        /etc/ssl/private/key.pem\n-\nadd: olcLogLevel\nolcLogLevel:
+      #        stats\n-\n"
+      #
+      #        (compared using ==)
+      #
+      #        Diff:
+      #
+      #  @@ -1,12 +1,12 @@
+      #   dn: cn=config
+      #   changetype: modify
+      #  +replace: olcTLSCertificateFile
+      #  +olcTLSCertificateFile: /etc/ssl/certs/cert.pem
+      #  +-
+      #   replace: olcTLSCertificateKeyFile
+      #   olcTLSCertificateKeyFile: /etc/ssl/private/key.pem
+      #   -
+      #   add: olcLogLevel
+      #   olcLogLevel: stats
+      #  --
+      #  -replace: olcTLSCertificateFile
+      #  -olcTLSCertificateFile: /etc/ssl/certs/cert.pem
+      #   -
+      #
+      # ./spec/unit/puppet/provider/openldap_config_hash/olc_spec.rb:93
+
+      unless RUBY_VERSION =~ /^1\.8/
         expect(provider.create).to eq(create_ldif)
       end
     end
